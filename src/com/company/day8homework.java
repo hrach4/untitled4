9c91527848557cebbbbe75682c248df9ac9aadd1
Hrachya <Hrach900135@gmail.com>
2021-12-31 16:34:36 +0400

    a

--- src/com/company/day8homework.java
+++ src/com/company/day8homework.java
@@ -19,7 +19,16 @@ public class day8homework {
         }
 */
         //2
-
+/*
+        System.out.println("how many items in your list");
+        int[] nums = new int[scan.nextInt()];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scan.nextInt();
+        }
+        for (int i = 1; i < nums.length; i += 2) {
+            System.out.println(nums[i]);
+        }
+*/
         //3
 /*
         short s = 0;
@@ -186,8 +195,8 @@ public class day8homework {
             System.out.println(nums[i]);
         }
 */
-        //14
-
+        //14?????????????????????????????
+/*
         int[] nums = {13, 29, 63, 92, 62, 14, 4, 233, 19, 6, 10, 655, 42, 37,};
         System.out.println("enter the number of the item what you wont to remove");
         int indexofremove = scan.nextInt();
@@ -199,5 +208,41 @@ public class day8homework {
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
         }
+*/
+        //15
+/*
+        int[] nums = {1, 2, 2, 3, 3, 3,};
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i != j) {
+                    k = 1;
+                }
+            }
+            if (k == 1) {
+                k = 0;
+                continue;
+            }
+            System.out.println(nums[i]);
+        }
+*/
+        //16
+/*
+        int[] nums = {1, 0, 3, 0, 5, 0, 0, 0, 1, 0, 1};
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] != 0) {
+                        nums[i] = nums[j];
+                        nums[j] = 0;
+                        break;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+*/
     }
 }
